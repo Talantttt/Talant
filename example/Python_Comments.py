e06d87913f157efdb5cca227616dd9590cd84b89
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-21 17:14:57 +0600

    Update Python_Comments.py

--- example/Python_Comments.py
+++ example/Python_Comments.py
@@ -1,1 +1,18 @@
-
+Python Comments
+4)print("Hello, World!")
+#This is a comment.
+5)#This is a comment
+print("Hello, World!")
+6)print("Hello, World!") #This is a comment
+7)#print("Hello, World!")
+print("Cheers, Mate!")
+8)#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+9)"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
