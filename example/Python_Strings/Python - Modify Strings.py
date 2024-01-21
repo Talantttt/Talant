83a118afd5452245be100636c43e2b350bdc4db2
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-21 17:33:44 +0600

    Create Python - Modify Strings.py

---
+++ example/Python_Strings/Python - Modify Strings.py
@@ -0,0 +1,11 @@
+Python - Modify Strings
+59)a = "Hello, World!"
+print(a.upper())
+60)a = "Hello, World!"
+print(a.lower())
+61)a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+62)a = "Hello, World!"
+print(a.replace("H", "J"))
+63)a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
