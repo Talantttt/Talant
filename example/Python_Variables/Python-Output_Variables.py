fa8c27da8ff44b9f348bad6046b0f6f2abe153b3
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-21 17:26:36 +0600

    Create Python-Output_Variables.py

---
+++ example/Python_Variables/Python-Output_Variables.py
@@ -0,0 +1,20 @@
+Python - Output Variables
+21)x = "Python is awesome"
+print(x)
+22)x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+23)x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+24)x = 5
+y = 10
+print(x + y)
+25)x = 5
+y = "John"
+print(x + y)
+26)x = 5
+y = "John"
+print(x, y)
