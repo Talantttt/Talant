c88a66d6750e6c77e67b8daee3bae2d1b2b4c9e7
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-21 17:25:43 +0600

    Create Python_Variables-Assign_Multiple_Values.py

---
+++ example/Python_Variables/Python_Variables-Assign_Multiple_Values.py
@@ -0,0 +1,14 @@
+Python Variables - Assign Multiple Values
+18)x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+19)x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+20)fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
