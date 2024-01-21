6c33bb7014cb2b7ecbb15763e394bfa11cd2b491
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-21 17:30:47 +0600

    Update Python_Numbers.py

--- example/Python_Numbers.py
+++ example/Python_Numbers.py
@@ -1,1 +1,58 @@
+Python Numbers
+32)x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+33)print(type(x))
+print(type(y))
+print(type(z))
+34)x = 1
+y = 35656222554887711
+z = -3255522
 
+print(type(x))
+print(type(y))
+print(type(z))
+35)x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+36)x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+37)x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+38)x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+39)import random
+
+print(random.randrange(1, 10))
