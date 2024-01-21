8f1f0985c46646cb80583d3161b29200ccc83db2
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-21 17:32:35 +0600

    Update and rename example/Python_Strings.py to example/Python_Strings/Strings.py

---
+++ example/Python_Strings/Strings.py
@@ -0,0 +1,31 @@
+Python Strings
+44)print("Hello")
+print('Hello')
+45)a = "Hello"
+print(a)
+46)a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+47)a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+48)a = "Hello, World!"
+print(a[1])
+49)for x in "banana":
+  print(x)
+50)a = "Hello, World!"
+print(len(a))
+51)txt = "The best things in life are free!"
+print("free" in txt)
+52)txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+53)txt = "The best things in life are free!"
+print("expensive" not in txt)
+54)txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
