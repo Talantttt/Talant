d0aedb01f7e8937565151e7bd35dbce96c007601
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-28 21:38:38 +0600

    Create Copy_Dictionaries.py

---
+++ example_lab2/Python_Dictionaries/Copy_Dictionaries.py
@@ -0,0 +1,18 @@
+#copy()
+thisdict={
+    "brand": "Ford",
+    "model": "Mustang",
+    "yeear": 1964
+}
+mydict=thisdict.copy()
+print(mydict)
+print("\n")
+
+#dict
+thisdict={
+    "brand": "Ford",
+    "model": "Mustang",
+    "yeear": 1964
+}
+mydict=dict(thisdict)
+print(mydict)
