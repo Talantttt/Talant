ab889b7204157193bed23a743b29d845a904240a
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-28 21:47:19 +0600

    Create Remove_Set_Items.py

---
+++ example_lab2/Python_Sets/Remove_Set_Items.py
@@ -0,0 +1,28 @@
+#Remove Item
+thisset={"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+print("\n")
+
+#discard()
+thisset={"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+print("\n")
+
+#pop()
+thisset={"appple", "banana", "cherry"}
+x=thisset.pop()
+print(x)
+print(thisset)
+print("\n")
+
+#clear()
+thisset={"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+print("\n")
+
+#del
+thisset={"apple", "banana", "cherry"}
+del thisset
