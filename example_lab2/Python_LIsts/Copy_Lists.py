3fa657ae5422e65ae9fc41484e0203f13a8f3842
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-28 21:43:23 +0600

    Create Copy_Lists.py

---
+++ example_lab2/Python_LIsts/Copy_Lists.py
@@ -0,0 +1,12 @@
+#Copy a List
+thislist=["apple", "banana", "cherry"]
+mylist=thislist.copy()
+print(mylist)
+print("\n")
+
+#list
+thislist=["apple", "banana", "cherry"]
+mylist=list(thislist)
+print(mylist)
+print("\n")
+
