3b1c6b7f0409af17c7c6567683c80765a27ebda1
Talantttt <144251182+Talantttt@users.noreply.github.com>
2024-01-28 21:36:47 +0600

    Create Python_While_Loops.py

---
+++ example_lab2/Python_While_Loops.py
@@ -0,0 +1,32 @@
+#The while Loop
+i=1
+while i<6:
+    print(i)
+    i+=1
+print("\n")
+
+#The break Statement
+i=1
+while i<6:
+    print(i)
+    if i==3:
+        break
+    i+=1
+print("\n")
+
+#The continue Statement
+i=0
+while i<6:
+    i+=1
+    if i==3:
+        continue
+    print(i)
+print("\n")
+
+#The else Statement
+i=1
+while i<6:
+    print(i)
+    i+=1
+else:
+    print("i is no longer less than 6")
